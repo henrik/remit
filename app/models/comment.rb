13ab868e5b9b4623e3cc2e67d157802f6d0ad459
Henrik Nyh <henrik@nyh.se>
2014-06-13 00:30:19 +0200

    Tweak #to_json for commit/comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ActiveRecord::Base
   serialize :payload, Hash
 
   scope :newest_first, -> { order("id DESC") }
+
+  def to_json
+    super(
+      except: [ :payload ],
+    )
+  end
 end
--- app/models/commit.rb
+++ app/models/commit.rb
@@ -2,4 +2,15 @@ class Commit < ActiveRecord::Base
   serialize :payload, Hash
 
   scope :newest_first, -> { order("id DESC") }
+
+  def short_sha
+    sha.first(10)
+  end
+
+  def to_json
+    super(
+      except: [ :payload ],
+      methods: [ :short_sha ],
+    )
+  end
 end
