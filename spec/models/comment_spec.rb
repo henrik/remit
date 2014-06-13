8364e1f1f1876ba76d0718032b0ca44b5f7162c2
Henrik Nyh <henrik@nyh.se>
2014-06-13 08:34:41 +0200

    Spec Comment#as_json

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Comment, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Comment, "#as_json" do
+  it "includes some attributes" do
+    comment = Comment.new(id: 123)
+    expect(comment.as_json).to include "id" => 123
+  end
+
+  it "excludes the payload" do
+    comment = Comment.new(payload: { foo: "bar" })
+
+    expect(comment.as_json).not_to have_key "payloay"
+  end
 end
