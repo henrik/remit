b96062acc087f852061c3d94db5b5c9e7f4474d2
Henrik Nyh <henrik@nyh.se>
2014-06-14 10:10:27 +0200

    Remove jQuery.
    Add it back if we need it. AngularJS bundles its jqLite dependency.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require angular
 //= require angular-route
 //= require_tree ./vendor
