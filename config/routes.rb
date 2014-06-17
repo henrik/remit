4192b4a559b77a377cebd66d92f4c9faabc42187
Henrik Nyh <henrik@nyh.se>
2014-06-17 17:39:03 +0200

    html5Mode URLs (no hashbangs)
    Less risk of putting query params in the wrong place.
    
    Fixes #11.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root 'pages#index'
   resource :github_webhook, only: [ :create ]
+
   post "/commits/:id/reviewed" => 'commits#reviewed'
   delete "/commits/:id/unreviewed" => 'commits#unreviewed'
+
+  root 'pages#index'
+
+  # Catch-all since we don't use hashbang URLs; all paths should
+  # render the same page and then we route in JS.
+  get '*path' => 'pages#index'
 end
