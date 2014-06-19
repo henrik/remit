434dd0229e8bd3f67e06822ff1a35b9163dab7ef
Henrik Nyh <henrik@nyh.se>
2014-06-20 00:09:10 +0200

    Lower record limit for speed
    Started feeling a bit sluggish. Should look into that, but until then let's just lower this.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < UserFacingController
   # The number of records to show (on load and after updates).
   # The reason for a limit is that we expect (but haven't verified) it could get slow.
-  MAX_RECORDS = 250
+  MAX_RECORDS = 200
 
   def index
     render locals: {
