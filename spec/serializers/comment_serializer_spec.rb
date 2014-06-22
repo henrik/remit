fba7f404d8adbdf8ec2960815be69eec7a75ce75
Henrik Nyh <henrik@nyh.se>
2014-06-22 21:48:58 +0200

    Clean up serializer specs

--- spec/serializers/comment_serializer_spec.rb
+++ spec/serializers/comment_serializer_spec.rb
@@ -1,21 +1,8 @@
 require "rails_helper"
 
 describe CommentSerializer, "#as_json" do
-  let(:comment) {
-    build(:comment,
-      commit_sha: "faa",
-      body: "Yo.",
-      author: Author.new(username: "charlesbabbage"),
-    )
-  }
-
+  let(:comment) { build(:comment) }
   let(:serializer) { CommentSerializer.new(comment) }
-
-  it "includes the desired attributes" do
-    expect(serializer.as_json).to include(
-      body: "Yo.",
-    )
-  end
 
   it "camelizes keys" do
     expect(serializer.as_json.keys).to include(:authorName)
--- spec/serializers/commit_serializer_spec.rb
+++ spec/serializers/commit_serializer_spec.rb
@@ -1,15 +1,9 @@
 require "rails_helper"
 
 describe CommitSerializer, "#as_json" do
-  let(:commit) { build(:commit, reviewed_at: Time.now, created_at: Time.now) }
+  let(:commit) { build(:commit, created_at: Time.now) }
 
   let(:serializer) { CommitSerializer.new(commit) }
-
-  it "builds a hash of properties" do
-    expect(serializer.as_json).to include(
-      reviewed: true,
-    )
-  end
 
   it "camelizes keys" do
     expect(serializer.as_json.keys).to include(:authorName)
