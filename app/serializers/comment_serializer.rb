92166b0b83d057aa53743f4dbc529306d642fd05
Henrik Nyh <henrik@nyh.se>
2014-06-22 21:15:43 +0200

    Cleanup.

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -4,10 +4,10 @@ class CommentSerializer < ActiveModel::Serializer
   attributes :github_id,
     :body,
     :author_name,
+    :author_email,
     :url,
     :commit_sha,
     :commit,
-    :author_email,
     :timestamp
 
   private
@@ -16,13 +16,13 @@ class CommentSerializer < ActiveModel::Serializer
     comment.author.name_or_username
   end
 
-  def commit
-    comment.commit.try(:as_json)
-  end
-
   # May be nil; we don't get a lot of data with comments.
   def author_email
     comment.author.email
+  end
+
+  def commit
+    comment.commit.try(:as_json)
   end
 
   alias_method :comment, :object
