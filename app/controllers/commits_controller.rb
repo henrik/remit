71bb5d1651f3aeb0526a1158a6717668e3c08abb
Henrik Nyh <henrik@nyh.se>
2014-06-22 22:32:20 +0200

    Cleanup

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -3,20 +3,15 @@
 
 class CommitsController < UserFacingController
   def reviewed
-    id = params[:id].to_i
-    email = params[:email]
-
-    commit = Commit.find(id)
-    commit.mark_as_reviewed_by(email)
+    commit = Commit.find(params[:id])
+    commit.mark_as_reviewed_by(params[:email])
     push_event "commit_reviewed", commit
 
     render text: "OK"
   end
 
   def unreviewed
-    id = params[:id].to_i
-
-    commit = Commit.find(id)
+    commit = Commit.find(params[:id])
     commit.mark_as_unreviewed
     push_event "commit_unreviewed", commit
 
