a914cc865c832cb5ffcc63d56d26643d54290c64
Henrik Nyh <henrik@nyh.se>
2014-06-29 20:18:20 +0200

    Doc

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -3,6 +3,7 @@ class ApplicationSerializer < ActiveModel::Serializer
 
   private
 
+  # Convert keys from snake_case to lowerCamelCase.
   # https://github.com/rails-api/active_model_serializers/issues/398
   def attributes
     super.map { |k, v| [ k.to_s.camelize(:lower).to_sym, v ] }.to_h
