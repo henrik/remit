39482f9a4f7a58a11597df876c7766b7dcb296cf
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2015-06-04 16:20:28 +0200

    Simplify
    Copy pasted a bit too much. We don't run multiple test processes in this project.

--- config/initializers/message_bus.rb
+++ config/initializers/message_bus.rb
@@ -3,7 +3,7 @@ url = ENV["REDISCLOUD_URL"]
 if ENV["DEVBOX"]
   # Redis supports multiple databases, we normally use 0, but tests use 1 and up
   # so that they can use the same redis server but not collide with the regular database.
-  database_number = Rails.env.test? ? (ENV["TEST_ENV_NUMBER"].to_i + 1) : 0
+  database_number = Rails.env.test? ? 1 : 0
   url = "redis://127.0.0.1:#{`service_port redis`.chomp}/#{database_number}"
 end
 
