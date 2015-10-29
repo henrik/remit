a58b66933dbb384123d3f9279178c62c802fac2a
Henrik Nyh <henrik@nyh.se>
2015-10-29 14:34:18 +0100

    API: include oldest_age_in_seconds

--- app/controllers/api/v1/unreviewed_commits_controller.rb
+++ app/controllers/api/v1/unreviewed_commits_controller.rb
@@ -3,11 +3,15 @@ class Api::V1::UnreviewedCommitsController < UsersBaseController
     commits = Commit.for_index.unreviewed
 
     # Commits are not guaranteed to be ordered by timestamp, so we do this.
-    oldest_timestamp = commits.map(&:timestamp).min_by { |ts| Time.parse(ts) }
+    oldest_time = commits.map { |c| Time.parse(c.timestamp) }.min
+
+    oldest_timestamp = oldest_time && oldest_time.iso8601
+    oldest_age = oldest_time && (Time.now - oldest_time).to_i
 
     render json: {
       count: commits.count,
       oldest_timestamp: oldest_timestamp,
+      oldest_age_in_seconds: oldest_age,
     }
   end
 end
--- spec/requests/api_spec.rb
+++ spec/requests/api_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe "GET /api/v1/unreviewed_commits" do
-  it "tells you the number of unreviewed commits and the age of the oldest one" do
+  it "provides info about unreviewed commits" do
     # Oldest, but reviewed.
     create(:commit, :reviewed, payload: { timestamp: "2015-01-01T12:00:00Z" })
 
@@ -15,10 +15,22 @@ describe "GET /api/v1/unreviewed_commits" do
 
     expect(response).to be_success
 
-    expect(parsed_response).to eq(
+    expect(parsed_response).to include(
       "count" => 2,
       "oldest_timestamp" => "2015-01-01T13:00:00Z",
     )
+  end
+
+  it "includes the oldest_age_in_seconds" do
+    timestamp = 1.minute.ago
+
+    create(:commit, :unreviewed, payload: { timestamp: timestamp.iso8601 })
+
+    get "/api/v1/unreviewed_commits"
+
+    expect(response).to be_success
+
+    expect(parsed_response["oldest_age_in_seconds"]).to be_within(10).of(60)
   end
 
   it "handles 0 unreviewed commits" do
@@ -29,6 +41,7 @@ describe "GET /api/v1/unreviewed_commits" do
     expect(parsed_response).to eq(
       "count" => 0,
       "oldest_timestamp" => nil,
+      "oldest_age_in_seconds" => nil,
     )
   end
 
